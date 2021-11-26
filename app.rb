2a58903fbdcfb1df025a4539ebe8df300e7faa79
mcchama <aoe2_emi@hotmail.com>
2021-11-25 19:20:35 -0600

    Changed logic of the exersice
    Now the cells aren't limited by the edges of the table.
    That is to say, cells can appear live in the edges
    
    Co-authored-by: Josue Samano <Joshsam31@users.noreply.github.com>
    Co-authored-by: Armani <armanirmr@users.noreply.github.com>
    Co-authored-by: Irene Hernández <IrenehMDev@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -1,34 +1,20 @@
 =begin
-    Declaration of variables
-    Table dimension = 8 x 4
+    initTable -> Initialize the table with the cells
+    Params
+        null
+    Return
+        null
 =end
-# table = [[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]]
-#table [1][3] = 1
-x_length = 8
-y_length = 4
-table = []
-#table = Array.new(4, [rand(2),rand(2),rand(2),rand(2),rand(2),rand(2),rand(2),rand(2)])
-
-# table.each do |row|
-#     puts "#{row}"
-# end
-# # Logic
-# puts rand(2)
-
-for i in (0...y_length)
-    row = []
-    if i == 0 || i == y_length - 1
-        row = [0,0,0,0,0,0,0,0]
-    else
+def initTable(x_length = 8, y_length = 4)
+    table = []
+    for i in (0...y_length)
+        row = []
         for j in (0...x_length)
-            if j == 0 || j == x_length -1
-                row[j] = 0
-            else
-                row[j] = rand(2)
-            end
+            row[j] = rand(2)
         end
+        table.push(row)
+        print "#{table[i]}"
+        print "\n"
     end
-    table.push(row)
-    print "#{table[i]}"
-    print "\n"
+    return table
 end
