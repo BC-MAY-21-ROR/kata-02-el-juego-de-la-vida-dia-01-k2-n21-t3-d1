9ba0167c5f1618b65d463de62a7bb46e46d4d69f
mcchama <aoe2_emi@hotmail.com>
2021-11-25 19:22:54 -0600

    Get the neighbour count
    Now is possible to obtain the numbers of neighbours at some position ni the table.
    
    Co-authored-by: Josue Samano <Joshsam31@users.noreply.github.com>
    Co-authored-by: Armani <armanirmr@users.noreply.github.com>
    Co-authored-by: Irene Hernández <IrenehMDev@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -18,3 +18,27 @@ def initTable(x_length = 8, y_length = 4)
     end
     return table
 end
+
+=begin
+    getNeighbourCount -> Check neighbour count
+    Params
+        x -> int: Position at x axis in the matrix
+        y -> int: Position at y axis in the matrix
+    Return
+        neighbourCount -> int: Count of near neighbours
+=end
+def getNeighbourCount(x, y, matrix)
+    puts "x:#{x} y:#{y}"
+    puts matrix[y][x]
+    y1_neighbour = matrix[y-1][x]
+    y2_neighbour = matrix[y+1][x]
+    puts "y1:#{y1_neighbour} y2:#{y2_neighbour}"
+
+    x1_neighbour = matrix[y][x-1]
+    x2_neighbour = matrix[y][x+1]
+    puts "x1:#{x1_neighbour} x2:#{x2_neighbour}"
+
+    return (y1_neighbour + y2_neighbour + x1_neighbour + x2_neighbour)
+end
+myTable = initTable
+puts "Total de vecinos #{getNeighbourCount(1,2, myTable)}"
