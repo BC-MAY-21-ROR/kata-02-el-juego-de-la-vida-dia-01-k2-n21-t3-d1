6218bee39e3f8a98073e030fcf6ec4c520957d47
mcchama <aoe2_emi@hotmail.com>
2021-11-25 21:59:57 -0600

    Exercise finished
    Now the cells go through a generation.
    Depending the conditions established will be the number of cells alive
    
    Co-authored-by: Josue Samano <Joshsam31@users.noreply.github.com>
    Co-authored-by: Armani <armanirmr@users.noreply.github.com>
    Co-authored-by: Irene Hernández <IrenehMDev@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -1,20 +1,27 @@
+$x_length = 8
+$y_length = 4
+
+def printTable(table)
+    for i in (0...$y_length)
+        print "#{table[i]}"
+        print "\n"
+    end
+end
 =begin
     initTable -> Initialize the table with the cells
     Params
         null
     Return
-        null
+        arr[arr]
 =end
-def initTable(x_length = 8, y_length = 4)
+def initTable()
     table = []
-    for i in (0...y_length)
+    for i in (0...$y_length)
         row = []
-        for j in (0...x_length)
+        for j in (0...$x_length)
             row[j] = rand(2)
         end
         table.push(row)
-        print "#{table[i]}"
-        print "\n"
     end
     return table
 end
@@ -24,21 +31,69 @@ end
     Params
         x -> int: Position at x axis in the matrix
         y -> int: Position at y axis in the matrix
+        matrix -> arr[arr]: Cell table
     Return
         neighbourCount -> int: Count of near neighbours
 =end
-def getNeighbourCount(x, y, matrix)
-    puts "x:#{x} y:#{y}"
-    puts matrix[y][x]
-    y1_neighbour = matrix[y-1][x]
-    y2_neighbour = matrix[y+1][x]
-    puts "y1:#{y1_neighbour} y2:#{y2_neighbour}"
-
-    x1_neighbour = matrix[y][x-1]
-    x2_neighbour = matrix[y][x+1]
-    puts "x1:#{x1_neighbour} x2:#{x2_neighbour}"
+def getNeighbourCount(y, x, matrix)
+    x1_neighbour = if x == 0 then 0 else matrix[y][x-1] end
+    x2_neighbour = if x == $x_length - 1 then 0 else matrix[y][x+1] end
+    y1_neighbour = if y == 0 then 0 else matrix[y-1][x] end
+    y2_neighbour = if y == $y_length - 1 then 0 else matrix[y+1][x] end
 
     return (y1_neighbour + y2_neighbour + x1_neighbour + x2_neighbour)
 end
+
+=begin
+    1 -> Cell alive; 0 -> cell dead
+
+    First gen
+    [0, 1, 0]
+    [1, 0, 1]   At  (1,1) 0 has 3 neighbours, so in the next gen it'll become 1
+    [0, 0, 0]
+
+    Second gen
+    [0, 1, 0]
+    [1, 1, 1]   At  (1,1) 1 has 3 neighbours, so in the next gen it'll become 0
+    [0, 0, 0]
+
+    Third gen
+    [0, 1, 0]
+    [1, 0, 1]
+    [0, 0, 0]
+
+    A cell is counter as a neighbour in the next conditions
+    [X, N, X]
+    [N, C, N]   At (1,1) C has valid neighbours N; X aren't considered as neighbors
+    [X, N, X]
+=end
+def checkCells(table)
+    newTable = []
+    for i in (0...$y_length)
+        newRow = []
+        for j in (0...$x_length)
+            neighbourCount = getNeighbourCount(i, j, table)
+            if neighbourCount == 3
+                newRow[j] = 1
+            elsif neighbourCount < 2
+                newRow[j] = 0
+            elsif neighbourCount > 3 && table[i][j] == 1
+                newRow[j] = 0
+            elsif neighbourCount  == 2 || neighbourCount == 3
+                newRow[j] = 1
+            end
+        end
+        newTable.push(newRow)
+    end
+    return newTable
+end
+
+puts "First gen"
 myTable = initTable
-puts "Total de vecinos #{getNeighbourCount(1,2, myTable)}"
+printTable(myTable)
+puts "\n"
+
+puts "Second gen"
+myNewTable = checkCells(myTable)
+printTable(myNewTable)
+puts "\n"
